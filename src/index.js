d006752f6c19b35da57ad38a591b004bb664fb47
unknown <benjaminforrest@protonmail.com>
2023-07-10 09:26:58 -0700

    added react router. added pages to hold apps for the separate pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React, { StrictMode } from "react";
+import { ReactDOM } from "react-dom/client";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home.js";
+import Inventory from "./Inventory.js";
+import About from "./About.js";
+import Privacy from "./Privacy.js";
+import Terms from "./Terms.js";
+import Support from "./Support.js";
 import "./styles.css";
 
 import App from "./App";
@@ -7,6 +15,16 @@ import App from "./App";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="inventory" element={<Inventory/>} />
+      <Route path="support" element={<Support/>} />
+      <Route path="about" element={<About/>} />
+      <Route path="/about/privacypolicy" element={<Privacy/>} />
+      <Route path="/about/termsandconditions" element={<Terms/>} />
+    </Routes>
+    </BrowserRouter>
     <App />
   </StrictMode>
 );
